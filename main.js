7d603c9df3ac30838d64decd380bd080df59f5a2
HarshitaDebapriya <83499509+HarshitaDebapriya@users.noreply.github.com>
2021-06-16 18:04:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var first_Name="Harshita"
+var second_name="debapriya"
+var ha=first_Name.concat(second_name);
+console.log(ha);
+var lengthofName= ha.length;
+console.log(lengthofName);
+var ha1="my name is harshita"
+var replacename=ha1.replace("is","a");
+console.log(replacename);
+var capitalname="CAT";
+var lowercase=capitalname.toLowerCase();
+console.log(lowercase);
+var uppercase=lowercase.toUpperCase();
+console.log(uppercase);
+var squareroot=Math.sqrt(16);
+console.log(squareroot);
+var maximumnumber=Math.max(3,4);
+console.log(maximumnumber);
+var minimumnumber=Math.min(7,8);
+console.log(minimumnumber);
+var power=Math.pow(4,2);
+console.log(power);
+
+
